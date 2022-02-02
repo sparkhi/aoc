1f4cbd7942b6db455186de531282352116b310f9
sparkhi <parkhi@gmail.com>
2022-02-02 17:45:32 +0000

    Day3 part 1 and 2 complete

--- src/main/scala/com/knowingwhere/aoc/Day3.scala
+++ src/main/scala/com/knowingwhere/aoc/Day3.scala
@@ -4,34 +4,62 @@ import scala.io.Source
 
 object Day3 {
   def getEpsilonRate(lines: List[String]): Int = {
-    val epsilonStringFull = (~getGammaRate(lines)).toBinaryString
-    Integer.parseInt(epsilonStringFull.substring(epsilonStringFull.length - 12), 2)
+    val epsilonStringFull = generateGammaRateString(lines,"").replace('0', 'X').replace('1', '0').replace('X', '1')
+    Integer.parseInt(epsilonStringFull, 2)
   }
-
 
   def getGammaRate(lines: List[String]): Int = {
-    val gammaString = getCommonDigit(lines, 0) + getCommonDigit(lines, 1) + getCommonDigit(lines, 2) +
-      getCommonDigit(lines, 3) + getCommonDigit(lines, 4) + getCommonDigit(lines, 5) +
-      getCommonDigit(lines, 6) + getCommonDigit(lines, 7) + getCommonDigit(lines, 8) +
-      getCommonDigit(lines, 9) + getCommonDigit(lines, 10) + getCommonDigit(lines, 11)
-    Integer.parseInt(gammaString, 2)
+    Integer.parseInt(generateGammaRateString(lines, ""), 2)
   }
 
-  def getCommonDigit(lines: List[String], index: Int): String = {
-    val lineSize = lines.size
-    val sum = lines.map(line => line.substring(index, index + 1).toInt).sum
-    if (sum >= lineSize / 2) {
-      "1"
+
+  def generateGammaRateString(lines: List[String], prefix: String): String = {
+    val index = prefix.length
+    val commonDigit = getCommonDigit(lines, index, "0")
+    val retVal = prefix + commonDigit
+    if (retVal.length == lines.head.length) {
+      retVal
     } else {
-      "0"
+      generateGammaRateString(lines, retVal)
     }
   }
 
+  def getCommonDigit(lines: List[String], index: Int, preferred: String): String = {
+    val lineSize = lines.size
+    val count1 = lines.count(line => line.substring(index, index + 1).equals("1"))
+    count1 match {
+      case x if x > (lineSize / 2f) => "1"
+      case x if x < (lineSize / 2f) => "0"
+      case _ => preferred
+    }
+  }
+
+  def getOxygenRating(lines: List[String], index: Int): Int = {
+    val common = getCommonDigit(lines, index, "1")
+    val remaining = lines.filter(line => line.substring(index, index + 1).equals(common))
+    if (remaining.size > 1) {
+      getOxygenRating(remaining, index + 1)
+    } else {
+      Integer.parseInt(remaining.head, 2)
+    }
+  }
+
+  def getCo2Rating(lines: List[String], index: Int): Int = {
+    val common = getCommonDigit(lines, index, "1")
+    val remaining = lines.filter(line => !line.substring(index, index + 1).equals(common))
+    if (remaining.size > 1) {
+      getCo2Rating(remaining, index + 1)
+    } else {
+      Integer.parseInt(remaining.head, 2)
+    }
+  }
   def main(args: Array[String]): Unit = {
     val resource = Source.fromResource("day3-input.txt")
     val lines = resource.getLines.toList
 
     println(getGammaRate(lines) * getEpsilonRate(lines))
+
+    println(getOxygenRating(lines, 0) * getCo2Rating(lines, 0))
 
   }
 }
--- src/test/scala/com/knowingwhere/aoc/Day3Test.scala
+++ src/test/scala/com/knowingwhere/aoc/Day3Test.scala
@@ -21,11 +21,35 @@ class Day3Test extends WordSpec with BeforeAndAfterEach {
     )
 
     "get individual common digits " in {
-      Day3.getCommonDigit(lines, 0) shouldBe "1"
-      Day3.getCommonDigit(lines, 1) shouldBe "0"
-      Day3.getCommonDigit(lines, 2) shouldBe "1"
-      Day3.getCommonDigit(lines, 3) shouldBe "1"
-      Day3.getCommonDigit(lines, 4) shouldBe "0"
+      Day3.getCommonDigit(lines, 0, "0") shouldBe "1"
+      Day3.getCommonDigit(lines, 1, "0") shouldBe "0"
+      Day3.getCommonDigit(lines, 2, "0") shouldBe "1"
+      Day3.getCommonDigit(lines, 3, "0") shouldBe "1"
+      Day3.getCommonDigit(lines, 4, "0") shouldBe "0"
+    }
+
+    "get common digit " in {
+      val newLines = List(
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "11100",
+        "10000",
+        "11001"
+      )
+      Day3.getCommonDigit(newLines, 1, "1") shouldBe "0"
+    }
+
+    "get individual when preferred is used " in {
+      val newLines = List(
+        "010",
+        "011",
+        "010",
+        "011"
+      )
+      Day3.getCommonDigit(newLines, 2, "0") shouldBe "0"
+      Day3.getCommonDigit(newLines, 2, "1") shouldBe "1"
     }
 
     "get gamma rate " in {
@@ -34,6 +58,14 @@ class Day3Test extends WordSpec with BeforeAndAfterEach {
 
     "get epsilon rate " in {
       Day3.getEpsilonRate(lines) shouldBe 9
+    }
+
+    "get oxygen rate " in {
+      Day3.getOxygenRating(lines, 0) shouldBe 23
+    }
+
+    "get co2 rate " in {
+      Day3.getCo2Rating(lines, 0) shouldBe 10
     }
   }
 }
